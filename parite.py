6229486fdd677479c85da11be958ce94bc8d833a
tanorbessane <bessanetanor@gmail.com>
2022-01-13 14:04:04 +0100

    add des premiers algo

---
+++ parite.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import analysis.cvs as c_an
+import analysis.xml as x_an
+def main():
+    c_an.launch_analysis('current_mps.csv')
+    x_an.launch_analysis('SyceronBrut.xml')
+if __name__ == "__main__":
+    main()
